a3ddc56c14252964dd64161a40604358b8d70e8b
Puzan sakya <pujan@rigonepal.com>
2023-11-20 14:24:10 +0545

    make it work

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,6 +1,17 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import type { Preview } from "@storybook/react";
+import React from "react";
 
 const preview: Preview = {
+  decorators: [
+    (Story: any) => {
+      return (
+        <ChakraProvider>
+          <Story />
+        </ChakraProvider>
+      );
+    },
+  ],
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
--- src/pages/date-picker-xstate/index.stories.tsx
+++ src/pages/date-picker-xstate/index.stories.tsx
@@ -1,10 +1,8 @@
-import { Button, ChakraProvider, Container, Flex } from '@chakra-ui/react';
+import { Button, Flex } from '@chakra-ui/react';
 import type { Meta } from '@storybook/react';
 import NepaliDatepickerV2 from '.';
-// import ConnectForm from '../ConnectForm';
 import FormProvider from '../FormProvider';
 import ConnectForm from '../connect-form';
-// import { CALENDAR_MODE } from './components/nepali-date-picker copy/calendar-engine';
 
 const meta: Meta<typeof NepaliDatepickerV2> = {
   component: NepaliDatepickerV2,
@@ -13,111 +11,198 @@ const meta: Meta<typeof NepaliDatepickerV2> = {
 
 export default meta;
 
-export const Default = {
-  args: {
-    value: '2023-10-29',
-    disableDateBefore: '2023-08-29',
-    disableDateAfter: '2023-12-29',
-    is_dark: false,
-    is_nepali: true,
-  },
+const argsBase = {
+  value: '2023-10-29',
+  disableDateBefore: '2023-08-29',
+  disableDateAfter: '2023-12-29',
+  is_dark: false,
+  is_nepali: false,
+}
+
+export const EngDefault = {
+  args: argsBase,
 
   render: (args: any) => {
     return (
-      <ChakraProvider>
-        <FormProvider
-          onSubmit={(data: any) => {
-            console.log(JSON.stringify(data, null, 2));
-          }}
-          defaultValues={{
-            // default: args.date,
-            // empty: '',
-            // disabled: '',
-            composed: args.value,
-          }}
-        >
-          <ConnectForm>
-            {(formProps: any) => {
-              const {
-                setError,
-                trigger,
-                control,
-                formState: { errors },
-              } = formProps;
+      <FormProvider
+        onSubmit={(data: any) => {
+          console.log(JSON.stringify(data, null, 2));
+        }}
+        defaultValues={{
+          composed: args.value,
+        }}
+      >
+        <ConnectForm>
+          {(formProps: any) => {
+            const {
+              setError,
+              trigger,
+              control,
+              formState: { errors },
+            } = formProps;
 
-              const inputProps = {
-                control,
-                errors,
-                setError,
-                trigger,
-                dateType: args.dateType,
-                disableDateBefore: args.disableDateBefore,
-                disableDateAfter: args.disableDateAfter,
-              };
+            const inputProps = {
+              control,
+              errors,
+              setError,
+              trigger,
+              dateType: args.dateType,
+              disableDateBefore: args.disableDateBefore,
+              disableDateAfter: args.disableDateAfter,
+            };
 
-              return (
-                <Container
-                  maxW='xl'
-                  py={5}
-                  display='flex'
-                  flexDirection='column'
-                  gap={3}
+            return (
+              <Flex gap={2} direction="column">
+
+                <NepaliDatepickerV2.Default
+                  name='composed'
+                  label='Composed And RHF Controlled'
+                  disable_date_before={args.disableDateBefore}
+                  disable_date_after={args.disableDateAfter}
+                  is_dark={args.is_dark}
+                  is_nepali={args.is_nepali}
+                  required
+                  {...inputProps}
+                />
+
+                <Flex>
+                  <Button type='submit'>Submit</Button>
+                </Flex>
+              </Flex>
+            );
+          }}
+        </ConnectForm>
+      </FormProvider>
+    );
+  },
+};
+
+export const EnglishComposed = {
+  args: argsBase,
+
+  render: (args: any) => {
+    return (
+      <FormProvider
+        onSubmit={(data: any) => {
+          console.log(JSON.stringify(data, null, 2));
+        }}
+        defaultValues={{
+          composed: args.value,
+        }}
+      >
+        <ConnectForm>
+          {(formProps: any) => {
+            const {
+              setError,
+              trigger,
+              control,
+              formState: { errors },
+            } = formProps;
+
+            const inputProps = {
+              control,
+              errors,
+              setError,
+              trigger,
+              dateType: args.dateType,
+              disableDateBefore: args.disableDateBefore,
+              disableDateAfter: args.disableDateAfter,
+            };
+
+            return (
+              <Flex gap={2} direction="column">
+
+                <NepaliDatepickerV2
+                  name='composed'
+                  label='Composed And RHF Controlled'
+                  disable_date_before={args.disableDateBefore}
+                  disable_date_after={args.disableDateAfter}
+                  is_dark={args.is_dark}
+                  is_nepali={args.is_nepali}
+                  required
+                  {...inputProps}
                 >
+                  <NepaliDatepickerV2.FormControl>
+                    <Flex gap={2}>
+                      <NepaliDatepickerV2.FormLabel />
+                    </Flex>
+                    <NepaliDatepickerV2.Component />
+                    <NepaliDatepickerV2.HelperText />
+                    <NepaliDatepickerV2.ErrorLabel />
+                  </NepaliDatepickerV2.FormControl>
+                </NepaliDatepickerV2>
 
-                  {/* <NepaliDatepickerV2.Default
-                    name='default'
-                    label='Default And RHF Controlled'
-                    required
-                    {...inputProps}
-                  /> */}
+                <Flex>
+                  <Button type='submit'>Submit</Button>
+                </Flex>
+              </Flex>
+            );
+          }}
+        </ConnectForm>
+      </FormProvider>
+    );
+  },
+};
 
-                  {/* <NepaliDatepickerV2.Default
-                    name='disabled'
-                    label='Disable and enable date with RHF Controlled'
-                    disableDateBefore={args.disableDateBefore}
-                    disableDateAfter={args.disableDateAfter}
-                    required
-                    {...inputProps}
-                  /> */}
+export const EnglishDefaultUncontrolled = {
+  args: argsBase,
 
-                  <NepaliDatepickerV2
-                    name='composed'
-                    label='Composed And RHF Controlled'
-                    disable_date_before={args.disableDateBefore}
-                    disable_date_after={args.disableDateAfter}
-                    is_dark={args.is_dark}
-                    is_nepali={args.is_nepali}
-                    required
-                    {...inputProps}
-                  >
-                    <NepaliDatepickerV2.FormControl>
-                      <Flex gap={2}>
-                        <NepaliDatepickerV2.FormLabel />
-                      </Flex>
-                      <NepaliDatepickerV2.Component />
-                      <NepaliDatepickerV2.HelperText />
-                      <NepaliDatepickerV2.ErrorLabel />
-                    </NepaliDatepickerV2.FormControl>
-                  </NepaliDatepickerV2>
+  render: (args: any) => {
+    const inputProps = {
+      dateType: args.dateType,
+      disableDateBefore: args.disableDateBefore,
+      disableDateAfter: args.disableDateAfter,
+    };
 
-                  {/* <NepaliDatepickerV2.Default
-                    name='uncontrolled'
-                    label='Uncontrolled'
-                    value={'2020-10-29'}
-                    onChange={(name: string, value: string) => {
-                      console.log({ name, value });
-                    }}
-                  /> */}
+    return (
+      <NepaliDatepickerV2.Default
+        name='composed'
+        label='Default Uncotrolled'
+        disable_date_before={args.disableDateBefore}
+        disable_date_after={args.disableDateAfter}
+        is_dark={args.is_dark}
+        is_nepali={args.is_nepali}
+        onChange={(data: string) => {
+          console.log(data)
+        }}
+        {...inputProps}
+      />
+    );
+  },
+};
 
-                  <Flex>
-                    <Button type='submit'>Submit</Button>
-                  </Flex>
-                </Container>
-              );
-            }}
-          </ConnectForm>
-        </FormProvider>
-      </ChakraProvider>
+export const EnglishComposedUncontrolled = {
+  args: argsBase,
+
+  render: (args: any) => {
+    const inputProps = {
+      dateType: args.dateType,
+      disableDateBefore: args.disableDateBefore,
+      disableDateAfter: args.disableDateAfter,
+    };
+
+    return (
+      <NepaliDatepickerV2
+        name='composed'
+        label='Composed And RHF Controlled'
+        disable_date_before={args.disableDateBefore}
+        disable_date_after={args.disableDateAfter}
+        is_dark={args.is_dark}
+        is_nepali={args.is_nepali}
+        onChange={(data: string) => {
+          console.log(data)
+        }}
+        {...inputProps}
+      >
+        <NepaliDatepickerV2.FormControl>
+          <Flex gap={2}>
+            <NepaliDatepickerV2.FormLabel />
+          </Flex>
+          <NepaliDatepickerV2.Component />
+          <NepaliDatepickerV2.HelperText />
+          <NepaliDatepickerV2.ErrorLabel />
+        </NepaliDatepickerV2.FormControl>
+      </NepaliDatepickerV2>
     );
   },
 };
